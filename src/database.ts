2596e5360a4ce415f63a646e1a11279b111f50b6
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2025-02-24 10:00:21 +0000

    commit

--- src/database.ts
+++ src/database.ts
@@ -5,9 +5,9 @@ export const AppDataSource = new DataSource({
     type: "postgres",
     host: Deno.env.get("DB_HOST") || "localhost",
     port: Number(Deno.env.get("DB_PORT")) || 5432,
-    username: Deno.env.get("DB_USER") || "postgres",
-    password: Deno.env.get("DB_PASSWORD") || "postgres",
-    database: Deno.env.get("DB_NAME") || "postgres",
+    username: Deno.env.get("DB_USER") ,
+    password: Deno.env.get("DB_PASSWORD") ,
+    database: Deno.env.get("DB_NAME") ,
     synchronize: true,
     logging: false,
     entities: [
